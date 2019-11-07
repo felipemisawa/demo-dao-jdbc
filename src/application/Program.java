424a332e97cb4dd9106d0e671fad5d099ff52506
Felipe Misawa <felipe.misawa@gmail.com>
2019-11-06 21:55:58 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,13 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
-import db.DB;
-import db.DbIntegrityException;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Department obj = new Department(1,"books");
+		System.out.println(obj);
 		
 	}
 
